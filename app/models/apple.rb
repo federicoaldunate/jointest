dea8e540b4408bcaa4aee5162dc1bf28d3059430
Eric O <eric@ejdigitalmedia.com>
2021-08-30 14:43:39 -0400

    Explicity specify join_table name on Apple/Zebra model relationships to EnabledDynamicField model (which fixes test failures)

--- app/models/apple.rb
+++ app/models/apple.rb
@@ -1,4 +1,4 @@
 class Apple < ApplicationRecord
-  has_and_belongs_to_many :enabled_dynamic_fields
+  has_and_belongs_to_many :enabled_dynamic_fields, join_table: 'apples_enabled_dynamic_fields'
   has_and_belongs_to_many :zebras
 end
--- app/models/zebra.rb
+++ app/models/zebra.rb
@@ -1,4 +1,4 @@
 class Zebra < ApplicationRecord
   has_and_belongs_to_many :apples
-  has_and_belongs_to_many :enabled_dynamic_fields
+  has_and_belongs_to_many :enabled_dynamic_fields, join_table: 'enabled_dynamic_fields_zebras'
 end
